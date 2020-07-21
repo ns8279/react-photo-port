e36f0dfda7689cb20d173ed9136baca7e7ca55c2
Nirupama Sundararaman <nirupama.2308@gmail.com>
2020-07-21 15:25:58 -0700

    hardcoding the Gallery images

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 //import logo from './logo.svg';
 import About from './components/About'
 import Nav from './components/Nav'
+import Gallery from './components/Gallery'
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
    <div>
      <Nav></Nav>
      <main>
+       <Gallery></Gallery>
        <About></About>
      </main>
    </div>
